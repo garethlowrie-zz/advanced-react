c9e40667cd879335e38e943500ea22dc487baf52
Gareth Lowrie <gareth.lowrie@communicatorcorp.com>
2018-10-22 16:19:53 +0100

    Started adding first query

--- sick-fits/frontend/components/Items/presentational.js
+++ sick-fits/frontend/components/Items/presentational.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-class Items extends Component {
-	render() {
-		return (
-			<div>
-				<p>Items!</p>
-			</div>
-		);
+const ALL_ITEMS_QUERY = gql`
+	query ALL_ITEMS_QUERY {
+		items {
+			id
+			title
+			price
+			description
+			image
+			largeImage
+		}
 	}
+`;
+
+const Items = () => {
+	return (
+		<div>
+			<p>Items!</p>
+			<Query query={ALL_ITEMS_QUERY}>
+				{(payload) => {
+					console.log(payload)
+					return <p>Hey im the child</p>
+				}}
+			</Query>
+		</div>
+	);
 }
 
 export default Items;
